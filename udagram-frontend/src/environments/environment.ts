bcd316a03d91eaedb5c33f34f9d2d572ef365d2b
Steve-ndeke <steve.ndeke@thejitu.com>
2022-09-17 10:39:29 +0300

    setup kubernetes on aws eks

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a320f1a79ed4246db9fad9f3407b6f3a-464837000.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
